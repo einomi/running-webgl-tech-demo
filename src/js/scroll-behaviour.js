07fc1a033cac24a13d449887232a3c53ded71716
Andrey Nikonov <a.einomi@gmail.com>
2023-12-24 18:38:44 +0100

    Enable scroll only if the page is fully loaded

--- src/js/scroll-behaviour.js
+++ src/js/scroll-behaviour.js
@@ -4,8 +4,14 @@ import ScrollSmoother from 'gsap/ScrollSmoother';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
-ScrollSmoother.create({
+const scrollSmoother = ScrollSmoother.create({
   smooth: 1.2, // how long (in seconds) it takes to "catch up" to the native scroll position
   effects: true, // looks for data-speed and data-lag attributes on elements
   smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
+});
+
+scrollSmoother.paused(true);
+
+window.addEventListener('load', () => {
+  scrollSmoother.paused(false);
 });
